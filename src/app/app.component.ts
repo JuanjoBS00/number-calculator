0fc746c975abf7fa666e89e598ca44ebb2929317
Juanjo <byjuanjo2000@gmail.com>
2022-03-25 10:32:21 +0000

    V1.0.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'number-calculator';
+  title = 'Calculadora básica';
+  text1="1";
+  text2="2";
+  result=0;
+
+  add():void{
+    var operator1 = Number(this.text1);
+    var operator2 = Number(this.text2);
+    this.result=operator1+operator2;
+  }
+  
+  substraction():void{
+    var operator1 = Number(this.text1);
+    var operator2 = Number(this.text2);
+    this.result=operator1-operator2;
+  }
+
+  multiply():void{
+    var operator1 = Number(this.text1);
+    var operator2 = Number(this.text2);
+    this.result=operator1*operator2;
+  }
+
+  divide():void{
+    var operator1 = Number(this.text1);
+    var operator2 = Number(this.text2);
+    this.result=operator1/operator2;
+  }
+
+  potency_squared():void{
+    var operator1 = Number(this.text1);
+    this.result=operator1*operator1;
+  }
+
+  potency():void{
+    var operator1 = Number(this.text1);
+    var operator2 = Number(this.text2);
+    this.result=operator1**operator2;
+  }
+
+  sqrt():void{
+    var operator1 = Number(this.text1);
+    this.result=Math.sqrt(operator1);
+  }
+
+  module():void{
+    var operator1 = Number(this.text1);
+    var operator2 = Number(this.text2);
+    this.result=operator1%operator2;
+  }
+
+  reset():void{
+    this.text1 = "0";
+    this.text2 = "0";
+    this.result = 0;
+  }
+
 }
